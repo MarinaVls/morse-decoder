d9d4411ee89bdf087e92c649a41f00784f7df7fc
Marina <marisha.volos@gmail.com>
2022-08-03 23:56:30 +0300

    feat: add decode function in  index.js

--- src/index.js
+++ src/index.js
@@ -37,8 +37,42 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function getSymbol(morse) {
+    for (const [key, value] of Object.entries(MORSE_TABLE)) {
+        if(`${key}` === morse){
+            return `${value}`
+        }
+      } 
+}
+
+function decode(str) {
+    let array = [];
+    let symbols = "";
+    
+
+    for (let i = 0; i < str.length; i += 10) {
+        array.push(str.substring(i, i + 10).replace(/\b0+/g, ''));
+    }
+
+    for(let arr=0; arr < array.length; arr ++){
+        let result = "";
+        if(array[arr] != "**********"){
+      for (let j = 0; j < array[arr].length; j += 2) {
+          if(array[arr].substring(j, j + 2) == "10"){
+              result+=".";
+          }else{
+              result+="-";
+          }
+      }
+    
+      symbols += getSymbol(result);
+    }else{
+        symbols += ' ';
+    }
+
+    }
+
+    return symbols;
 }
 
 module.exports = {
